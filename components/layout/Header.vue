875279b3f6b43a0d9fd00f3ec30bb67a0a66c49f
Marcel Lindig <ml@nullpilot.de>
2020-04-15 15:28:56 +0200

    clear default styles, add header

---
+++ components/layout/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="sticky inset-x-0 top-0 z-10 bg-white">
+    <div class="container mx-auto flex px-8 py-4 items-center justify-between">
+      <div class="logo">
+        <Logo></Logo>
+      </div>
+
+      <div class="menu">
+        <nuxt-link to="events/" class="menu-link">Event-Kalender</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/layout/Logo.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style class="scoped">
+.menu-link {
+  display: block;
+  color: theme('colors.gray.800');
+  font-weight: 600;
+  font-size: theme('fontSize.sm');
+  padding: theme('spacing.3') theme('spacing.4');
+  position: relative;
+}
+
+.menu-link::before {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: 0 theme('spacing.4');
+  border-top: 2px solid theme('colors.indigo.600');
+  transition: transform 150ms ease-out;
+  transform: scaleX(0);
+}
+
+.menu-link:hover::before {
+  transform: scaleX(1);
+}
+
+.menu-link:hover {
+  color: theme('colors.indigo.700');
+  font-weight: 600;
+}
+</style>
---
+++ components/layout/Logo.vue
@@ -0,0 +1,45 @@
+<template>
+  <nuxt-link class="flex items-center logo-link" to="/">
+    <svg
+      fill="transparent"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      class="w-10 h-10 logo mr-2"
+    >
+      <path
+        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+      ></path>
+    </svg>
+
+    <span class="mt-1 font-bold">Jena Events</span>
+  </nuxt-link>
+</template>
+<style scoped>
+.logo-link {
+  animation: 1s appear;
+}
+
+.logo {
+  transition: fill 150ms ease-out;
+}
+
+.logo-link:hover {
+  color: theme('colors.indigo.700');
+}
+
+.logo-link:hover .logo {
+  fill: theme('colors.indigo.100');
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
