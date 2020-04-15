cd5df40ed4f53ebd234cbae90437b6ac67890821
Marcel Lindig <ml@nullpilot.de>
2020-04-15 17:57:21 +0200

    switch to express

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
+const express = require('express')
+const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-const fastify = require('fastify')({
-  logger: true
-})
+const app = express()
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
+// Import API routes
+const apiRoutes = require('./api.js')
+
 async function start() {
-  // Instantiate nuxt.js
+  // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const {
-    host = process.env.HOST || '127.0.0.1',
-    port = process.env.PORT || 3000
-  } = nuxt.options.server
+  const { host, port } = nuxt.options.server
 
   await nuxt.ready()
   // Build only in dev mode
@@ -23,14 +23,15 @@ async function start() {
     await builder.build()
   }
 
-  fastify.use(nuxt.render)
+  // Give nuxt middleware to express
+  app.use('/api', apiRoutes)
+  app.use(nuxt.render)
 
-  fastify.listen(port, host, (err, address) => {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
   })
 }
-
 start()
