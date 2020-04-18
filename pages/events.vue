0c05aedd26202d7fe076ac74d3f40fb94c8e25b3
Marcel Lindig <ml@nullpilot.de>
2020-04-18 02:25:15 +0200

    scaffold host view and api

---
+++ pages/events.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Section>
+      <h1 class="text-4xl font-bold text-indigo-600 mb-8">
+        Veranstaltungsplaner
+      </h1>
+
+      <EventListBase start="2020-01-01" end="2020-12-31" :admin="true" />
+    </Section>
+  </div>
+</template>
+
+<script>
+import Section from '~/components/layout/Section.vue'
+import EventListBase from '~/components/calendar/EventListBase.vue'
+
+export default {
+  components: {
+    Section,
+    EventListBase
+  }
+}
+</script>
